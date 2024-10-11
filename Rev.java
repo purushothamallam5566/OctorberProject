6c0c191c83360ecdc52b8802544c2cf689bb6d02
purushotham <purushotham>
2024-10-11 13:45:59 +0530

    this my first commit

---
+++ Rev.java
@@ -0,0 +1,15 @@
+public class Rev
+public static void main(String[] args) {
+		String name="purushotham";
+		String rev="";
+		int length=name.length();
+		for(int i=length-1;i>=0;i--)
+		{
+			rev=rev+name.charAt(i);
+			
+		}
+		
+		
+		System.out.println(rev);
+
+}}
