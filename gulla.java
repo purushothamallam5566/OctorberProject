f4eecbb816f627a06cc1d5ca949c5b5ebdeb4fb9
purushotham <purushotham>
2024-10-11 19:27:08 +0530

    gulla sambba

---
+++ gulla.java
@@ -0,0 +1,11 @@
+public class Rev
+public static void main(String[] args) {
+		String name="purushotham";
+		String rev="";
+		int length=name.length();
+		System.out.println(rev);
+		System.out.println(rev);		
+		System.out.println(rev);
+		System.out.println(rev);// lilly sambba gulla
+
+}}
